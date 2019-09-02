2469751e7219bea5522618b3ef13d909ff0adb33
Julia Guskova <julia.guskova.ext@opencascade.com>
2019-09-02 21:38:37 +0300

    Added files

--- TriangleWork/Program.cs
+++ TriangleWork/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Triangles;
 
 namespace TriangleWork
 {
   class Program
   {
+    static double SetPoint(string msg)
+    {
+      Console.WriteLine(msg);
+      return Convert.ToDouble(Console.ReadLine());
+    }
+
     static void Main(string[] args)
     {
+      double a, b, c;
+      a = SetPoint("Введите точку а");
+      b = SetPoint("Введите точку b");
+      c = SetPoint("Введите точку c");
+
+      TriangleUtils utils = new TriangleUtils();
+      TriangleUtils.TriangleType type = utils.GetTriangleType(a, b, c);
+
+      Console.WriteLine("Тип треугольника: " + type);
+      Console.ReadKey();
     }
   }
 }
