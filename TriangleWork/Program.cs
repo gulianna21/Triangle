11e3200a0d63bd6aeafe5d2ffdb3bf4388d57d8e
Julia Guskova <julia.guskova.ext@opencascade.com>
2019-09-08 20:59:16 +0300

    Added check on input

--- TriangleWork/Program.cs
+++ TriangleWork/Program.cs
@@ -7,16 +7,26 @@ namespace TriangleWork
   {
     static double SetPoint(string msg)
     {
-      Console.WriteLine(msg);
-      return Convert.ToDouble(Console.ReadLine());
+      while (true)
+      {
+        Console.WriteLine(msg);
+        try
+        {
+          return Convert.ToDouble(Console.ReadLine());
+        }
+        catch (Exception e)
+        {
+          Console.WriteLine("Введено неправильное значение.");
+        }
+      }
     }
 
     static void Main(string[] args)
     {
       double a, b, c;
-      a = SetPoint("Введите точку а");
-      b = SetPoint("Введите точку b");
-      c = SetPoint("Введите точку c");
+      a = SetPoint("Введите точку а: ");
+      b = SetPoint("Введите точку b: ");
+      c = SetPoint("Введите точку c: ");
 
       TriangleUtils utils = new TriangleUtils();
       TriangleUtils.TriangleType type = utils.GetTriangleType(a, b, c);
